c038261a0725471d748794ed4278bc71d63debe1
yasseraitnasser <yasseraitnasser4@gmail.com>
2026-03-31 20:29:00 +0100

    add: relativenumber

--- init.lua
+++ init.lua
@@ -100,6 +100,7 @@ vim.g.have_nerd_font = false
 
 -- Make line numbers default
 vim.o.number = true
+vim.o.relativenumber = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 -- vim.o.relativenumber = true
