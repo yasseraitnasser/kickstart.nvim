b6b8544b0e943fcfe9a51d7b90968ec13e4b699d
yasseraitnasser <yasseraitnasser4@gmail.com>
2026-07-02 16:14:18 +0100

    refactor: zO to open all folds, zC to close all folds

--- lua/custom/plugins/folding.lua
+++ lua/custom/plugins/folding.lua
@@ -8,8 +8,8 @@ return {
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
 
-      vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'Open all folds' })
-      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'Close all folds' })
+      vim.keymap.set('n', 'zO', require('ufo').openAllFolds, { desc = 'Open all folds' })
+      vim.keymap.set('n', 'zC', require('ufo').closeAllFolds, { desc = 'Close all folds' })
 
       require('ufo').setup {
         provider_selector = function(bufnr, filetype, buftype) return { 'treesitter', 'indent' } end,
